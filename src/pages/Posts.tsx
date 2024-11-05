2417d896f954ceb4167d0f70361bdfc83ad4dc65
Arussel <anhminh05102004@gmail.com>
2024-11-05 02:42:58 -0500

    Update title

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -55,9 +55,9 @@ const Posts = () => {
     fetchPosts();
   }, [navigate]);
   const truncateToWords = (text:string, maxWords: number) => {
-    const words = text.split(/\s+/); // Split text into words
+    const words = text.split(/\s+/); 
     if (words.length > maxWords) {
-      return words.slice(0, maxWords).join(' ') + '...'; // Truncate and add ellipsis
+      return words.slice(0, maxWords).join(' ') + '...'; 
     }
     return text;
   };
